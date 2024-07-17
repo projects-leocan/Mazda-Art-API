142d036e94a732d87b3523c766bebbb23c6a19af
shweta-leocan <shweta.leocan@gmail.com>
2024-07-17 13:04:10 +0530

    change query to search anywhere from the full name

--- src/controller/artistControllers/searchArtistController.js
+++ src/controller/artistControllers/searchArtistController.js
@@ -13,7 +13,13 @@ exports.searchArtistController = async (req, res) => {
 
     // search by name and contact number
     // console.log("search text", search_text);
-    const query = `SELECT * FROM artist WHERE mobile_number ILIKE '${search_text}%' or mobile_number ILIKE '%${search_text}' or fname ILIKE '${search_text}%' or fname ILIKE '%${search_text}' or lname ILIKE '${search_text}%' or lname ILIKE '%${search_text}'`;
+    // const query = `SELECT * FROM artist WHERE mobile_number ILIKE '${search_text}%' or mobile_number ILIKE '%${search_text}' or fname ILIKE '${search_text}%' or fname ILIKE '%${search_text}' or lname ILIKE '${search_text}%' or lname ILIKE '%${search_text}'`;
+    const query = `SELECT *, COUNT(*) OVER() AS totalArtist 
+    FROM artist 
+    WHERE mobile_number ILIKE '${search_text}%' 
+       OR mobile_number ILIKE '%${search_text}' 
+       OR fname ILIKE '%${search_text}%' 
+       OR lname ILIKE '%${search_text}%' ORDER BY artist_id`;
     // console.log(`search query: ${query}`);
     pool.query(query, async (err, result) => {
       // console.log(`err: ${err}`);
@@ -26,9 +32,12 @@ exports.searchArtistController = async (req, res) => {
         });
       } else {
         // console.log(`response: ${JSON.stringify(result.rows)}`);
+        const totalArtist = result.rows[0]?.totalartist;
+        delete res.totalartist;
         res.status(200).send({
           success: true,
           message: "Data fetch successfully",
+          totalArtist: totalArtist,
           data: result.rows,
           statusCode: 200,
         });
