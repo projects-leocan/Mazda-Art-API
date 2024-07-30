9c1c90775df29d1ed879b124b21a610defc72807
shweta-leocan <shweta.leocan@gmail.com>
2024-07-30 16:28:20 +0530

    send grant uid and jury dull name, art title

--- src/controller/artSubmission/getArtworkCommentController.js
+++ src/controller/artSubmission/getArtworkCommentController.js
@@ -14,18 +14,26 @@ exports.getArtworkCommentController = async (req, res) => {
     // let query = `SELECT * FROM trasaction_detail`;
     // let query = `SELECT * FROM artist_comments WHERE artist_id = ${artist_id}`;
     // let query = `SELECT ac.*, a.admin_name FROM artist_comments as ac JOIN admin a ON a.admin_id = ac.admin_id WHERE artist_id = ${artist_id}`;
+    // let query =
+    //   admin_id === undefined
+    //     ? `SELECT * FROM artwork_comment WHERE jury_id=${jury_id} AND artwork_id=${artwork_id}`
+    //     : `SELECT * FROM artwork_comment WHERE artwork_id=${artwork_id}`;
+
     let query =
       admin_id === undefined
-        ? `SELECT * FROM artwork_comment WHERE jury_id=${jury_id} AND artwork_id=${artwork_id}`
-        : `SELECT * FROM artwork_comment WHERE artwork_id=${artwork_id}`;
+        ? `SELECT a.*, j.full_name, sd.art_title , sd.grant_id
+FROM public.artwork_comment a, public.jury j, public.submission_details sd 
+WHERE a.artwork_id = sd.id AND a.jury_id = j.id AND a.jury_id=${jury_id} AND a.artwork_id=${artwork_id}`
+        : `SELECT a.*, j.full_name, sd.art_title, sd.grant_id
+FROM public.artwork_comment a, public.jury j, public.submission_details sd 
+WHERE a.artwork_id = sd.id AND a.jury_id = j.id AND a.artwork_id=${artwork_id}`;
     if (isAll == undefined) {
       offset = (page_no - 1) * record_per_page;
       query += ` LIMIT ${record_per_page} OFFSET ${offset}`;
     }
-    console.log(`query: ${query}`);
     pool.query(query, async (err, result) => {
-      console.log(`err: ${err}`);
-      console.log(`result: ${JSON.stringify(result)}`);
+      // console.log(`err: ${err}`);
+      // console.log(`result: ${JSON.stringify(result)}`);
       if (err) {
         return res.status(500).send({
           success: false,
@@ -33,10 +41,18 @@ exports.getArtworkCommentController = async (req, res) => {
           statusCode: 500,
         });
       } else {
+        let data = result.rows[0]?.grant_id;
+        const grant_uid_query = `SELECT grant_uid from grants where grant_id=${data}`;
+        const grant_uid = await pool.query(grant_uid_query);
+
+        const finalData = {
+          data: result.rows,
+          grant_uid: grant_uid.rows[0].grant_uid,
+        };
         return res.status(200).send({
           success: true,
           message: "Comments get Successfully",
-          data: result.rows,
+          data: finalData,
           statusCode: 200,
         });
       }
