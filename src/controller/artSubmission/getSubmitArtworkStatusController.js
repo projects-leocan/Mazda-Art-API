1fddc04aed58a95a84bb97a393f5bce04d5a6c42
shweta-leocan <shweta.leocan@gmail.com>
2024-08-01 16:01:12 +0530

    send grant_uid and art title

--- src/controller/artSubmission/getSubmitArtworkStatusController.js
+++ src/controller/artSubmission/getSubmitArtworkStatusController.js
@@ -10,18 +10,21 @@ exports.getSubmitArtworkStatusController = async (req, res) => {
 
     let query =
       jury_id === undefined
-        ? `SELECT j.id, j.full_name, sd.status, sd.star_assigned, sd.comment 
+        ? `SELECT j.id, j.full_name, sd.status, sd.star_assigned, sd.comment, sd.art_title 
 FROM jury j LEFT JOIN submission_details sd ON sd.jury_id = j.id
 WHERE j.id IN 
 (SELECT jury_id FROM grant_assign 
 	WHERE grant_id in (SELECT grant_id FROM public.submission_details WHERE id = ${artwork_id}))`
-        : `SELECT j.id, j.full_name, sd.status, sd.star_assigned, sd.comment 
+        : `SELECT j.id, j.full_name, sd.status, sd.star_assigned, sd.comment, sd.art_title 
 FROM jury j LEFT JOIN submission_details sd ON sd.jury_id = j.id
 WHERE sd.jury_id = ${jury_id} AND j.id IN 
 (SELECT jury_id FROM grant_assign 
 	WHERE grant_id in (SELECT grant_id FROM public.submission_details WHERE id = ${artwork_id}))`;
 
     pool.query(query, async (err, result) => {
+      // console.log("result.rows", result.rows);
+      // console.log("query", query);
+
       if (err) {
         res.status(500).send({
           success: false,
@@ -29,6 +32,13 @@ WHERE sd.jury_id = ${jury_id} AND j.id IN
           statusCode: 500,
         });
       } else {
+        const grant_id_query = `SELECT grant_uid FROM grants WHERE grant_id in (SELECT grant_id FROM submission_details where id = ${artwork_id})`;
+        const grant_id_result = await pool.query(grant_id_query);
+        const grant_uid = grant_id_result.rows[0].grant_uid;
+
+        result.rows.map((row) => {
+          row.grant_uid = grant_uid;
+        });
         res.status(200).send({
           success: true,
           message: "Get Artwork Status Successfully",
