65ef5878b9031f3783030f39b144e58614f4ab99
shweta-leocan <shweta.leocan@gmail.com>
2024-08-08 19:18:39 +0530

    fix query

--- src/controller/grantMapping/getGrantAllSubmissionsController.js
+++ src/controller/grantMapping/getGrantAllSubmissionsController.js
@@ -35,21 +35,21 @@ exports.getGrantAllSubmissionsController = async (req, res) => {
     //   ORDER BY
     //     sd.artwork_id DESC;
     // `;
-    let query = `SELECT sd.*, g.grant_uid FROM
-    submission_details AS sd
-    JOIN
-    grants g ON sd.grant_id = g.grant_id
-   WHERE
-    sd.grant_id = ${grant_id}`;
+    //   let query = `SELECT sd.*, g.grant_uid FROM
+    //   submission_details AS sd
+    //   JOIN
+    //   grants g ON sd.grant_id = g.grant_id
+    //  WHERE
+    //   sd.grant_id = ${grant_id}`;
 
-    //   let query = `select * from submission_details
-    // where grant_id = ${grant_id}`;
+    let query = `select g.grant_uid, sb.* from submission_details as sb, grants g
+	where g.grant_id = sb.grant_id and sb.grant_id = ${grant_id}`;
 
     if (jury_id !== undefined) {
-      query += ` AND sd.jury_id = ${jury_id}`;
+      query += ` AND sb.jury_id = ${jury_id}`;
     }
 
-    query += ` ORDER BY sd.artwork_id DESC`;
+    query += ` ORDER BY sb.artwork_id DESC`;
 
     console.log("query", query);
 
