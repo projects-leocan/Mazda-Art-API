a382bb214da82fd1fe1f166a7660d4e98ac249ad
shweta-leocan <shweta.leocan@gmail.com>
2024-08-08 19:36:24 +0530

    issue fix

--- src/controller/artSubmission/getAllGrantSubmissionController.js
+++ src/controller/artSubmission/getAllGrantSubmissionController.js
@@ -26,36 +26,39 @@ exports.getAllGrantSubmissionController = async (req, res) => {
           // FROM submission_details as sd
           // JOIN grants g ON sd.grant_id = g.grant_id
           // JOIN artist a ON sd.artist_id = a.artist_id where jury_id=${jury_id} order by sd.submited_time DESC`;
-          `SELECT 
-    (SELECT COUNT(*) FROM submission_details) AS total_count, 
-    g.grant_uid, 
-    sd.artwork_id, 
-    sd.grant_id, 
-    sd.submited_time, 
-    sd.art_file, 
-    sd.art_title, 
-    sd.art_description, 
-    sd.height, 
-    sd.width, 
-    srd.status
-FROM 
-    submission_details AS sd
-JOIN 
-    grants g ON sd.grant_id = g.grant_id
-JOIN 
-    artist a ON sd.artist_id = a.artist_id
-JOIN 
-    submission_review_details srd ON sd.artwork_id = srd.artwork_id
-WHERE 
-    sd.jury_id = ${jury_id}
-ORDER BY 
-    sd.submited_time DESC
-`;
+          //           `SELECT
+          //     (SELECT COUNT(*) FROM submission_details) AS total_count,
+          //     g.grant_uid,
+          //     sd.artwork_id,
+          //     sd.grant_id,
+          //     sd.submited_time,
+          //     sd.art_file,
+          //     sd.art_title,
+          //     sd.art_description,
+          //     sd.height,
+          //     sd.width,
+          //     srd.status
+          // FROM
+          //     submission_details AS sd
+          // JOIN
+          //     grants g ON sd.grant_id = g.grant_id
+          // JOIN
+          //     artist a ON sd.artist_id = a.artist_id
+          // JOIN
+          //     submission_review_details srd ON sd.artwork_id = srd.artwork_id
+          // WHERE
+          //     sd.jury_id = ${jury_id}
+          // ORDER BY
+          //     sd.submited_time DESC
+          // `
+          `select (SELECT COUNT(*) FROM submission_details) AS total_count, g.grant_uid, sb.* from submission_details as sb, grants g
+	where g.grant_id = sb.grant_id and sb.jury_id = ${jury_id}`;
+
     if (isAll == undefined) {
       const offset = (page_no - 1) * record_per_page;
       query += ` LIMIT ${record_per_page} OFFSET ${offset}`;
     }
-    // console.log("queyr", query);
+    console.log("queyr------------", query);
 
     pool.query(query, async (err, result) => {
       console.log("err: ", err);
