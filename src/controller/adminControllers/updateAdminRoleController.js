b127443ef88fed1c6ecb0124ccecb0b6dceceb16
shweta-leocan <shweta.leocan@gmail.com>
2024-08-21 14:58:47 +0530

    1094 done

--- src/controller/adminControllers/updateAdminRoleController.js
+++ src/controller/adminControllers/updateAdminRoleController.js
@@ -9,12 +9,13 @@ exports.updateAdminRoleController = async (req, res) => {
     console.log("req body", req.body);
 
     // if (role_is_updated !== undefined) {
-    let deleteQuery = `DELETE FROM admin_role WHERE admin_id = ${admin_id}`;
-    const deleteResult = await pool.query(deleteQuery);
+    //
 
     if (!_.isEmpty(role)) {
       // Parse the JSON string into an array of objects
       //   const roleArray = JSON.parse(role);
+      let deleteQuery = `DELETE FROM admin_role WHERE admin_id = ${admin_id}`;
+      const deleteResult = await pool.query(deleteQuery);
       const roleArray = role;
       console.log("roleArray", roleArray);
       // Construct the values string for the INSERT query
@@ -38,6 +39,26 @@ exports.updateAdminRoleController = async (req, res) => {
           res.status(200).send({
             success: true,
             message: "Role Selected Successfully",
+            statusCode: 200,
+          });
+        }
+      });
+    } else {
+      let deleteQuery = `DELETE FROM admin_role WHERE admin_id = ${admin_id}`;
+      await pool.query(deleteQuery, async (err, result) => {
+        console.log("err in update role", err);
+        console.log("update role result", result);
+
+        if (err) {
+          res.status(500).send({
+            success: false,
+            message: err,
+            statusCode: 500,
+          });
+        } else {
+          res.status(200).send({
+            success: true,
+            message: "All Role Unselected Successfully",
             statusCode: 200,
           });
         }
